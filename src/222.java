a8d3ea1e5bb7b42118dc3b09ad4fd97515cef774
OuFin <ouyuqi666@gmail.com>
2024-01-24 16:22:05 +0800

    modify3

--- src/222.java
+++ src/222.java
@@ -1,7 +1,5 @@
 package src;
 
 public class 222 {
-    System.out.println("modify");
-    System.out.println("modify1");
-    System.out.println("modify2");
-}
+    System.out.println("modify3");
+    
